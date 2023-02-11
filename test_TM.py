97e60cd8312f9e6eceadbe66223cd00f258bc933
liyaozhang <zlysinap@hotmail.com>
2023-02-11 22:34:38 +0100

    add test_TM.py

---
+++ test_TM.py
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 runTableMaker.py configs/configTableMakerDataRun2.json -runData --arg table-maker:processBarrelOnlyWithCov:true event-selection-task:syst:pp internal-dpl-aod-reader:aod-file:@run2_conv_LHC17p_ForTM_sample.txt table-maker:cfgQA:true table-maker:cfgBarrelTrackCuts:jpsiO2MCdebugCuts2 --add_col_conv --add_fdd_conv 
